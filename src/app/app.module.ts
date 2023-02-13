46df685758313d133ea8c2d33f7874f60bc2cdca
jebalimahjoub <mahjoub_jebali@yahoo.fr>
2023-02-13 15:01:09 +0100

    la création d'une class produit et afficher la list avec ngFor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProduitComponent } from './produit/produit.component';
+import { ProduitMockService } from './produit/produit.mock.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProduitComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ProduitMockService],
+  bootstrap: [AppComponent] // point d'enter de l'application
 })
 export class AppModule { }
---
+++ src/app/produit/produit.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Produit } from '../shared/produit';
+import { ProduitMockService } from './produit.mock.service';
+
+@Component({
+  selector: 'app-produit',
+  templateUrl: './produit.component.html',
+  styleUrls: ['./produit.component.css']
+})
+export class ProduitComponent implements OnInit {
+
+  produits: Produit[] | undefined;
+  constructor(private produitService: ProduitMockService) { }
+
+  ngOnInit(): void {
+    this.produits = this.produitService.getProduits();
+  }
+
+}
